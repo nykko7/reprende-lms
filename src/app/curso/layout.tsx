be7d32a555973ee83221fd9e1296a894f8a0ab70
Nykko <n.perezbus@gmail.com>
2024-08-17 12:14:03 -0400

    fix: Change last course banner in layout

--- src/app/curso/layout.tsx
+++ src/app/curso/layout.tsx
@@ -8,9 +8,8 @@ const LandingLayout = ({ children }: { children: React.ReactNode }) => {
     <CTAButtonRefProvider>
       <div className="h-full selection:bg-primary selection:text-primary-foreground">
         <div className="bg-yellow-500 p-2 text-center text-sm text-black xs:p-3 xs:text-base">
-          La proxima generación del curso <strong> en vivo</strong> comienza el{" "}
-          <strong>Sábado 03 de Agosto</strong>. Quedan los{" "}
-          <strong>últimos cupos</strong>
+          La última generación del curso <strong> en vivo</strong> ya comenzó.{" "}
+          <strong>¡Aún te puedes inscribir!</strong>
         </div>
         <Navbar />
         {/* <Snowflakes numberOfSnowflakes={30} /> */}
